9a2408da226409673780f989252dc201f271ba45
alexsh69 <dfx.alex@gmail.com>
2020-03-28 19:38:01 +0300

    complete poc

---
+++ src/main/java/JvmbtMaven.java
@@ -0,0 +1,12 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class JvmbtMaven {
+
+    private static Logger logger = LoggerFactory.getLogger(JvmbtMaven.class);
+
+    public static void main(String [] args)
+    {
+        logger.info("Info log message");
+    }
+}
